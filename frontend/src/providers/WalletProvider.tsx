e11133feb558d3302f422039c4ab29123fc1e94d
logcho <lsjcwma@gmail.com>
2025-02-09 22:28:44 -0600

    chore: init rainbow

---
+++ frontend/src/providers/WalletProvider.tsx
@@ -0,0 +1,55 @@
+"use client";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+    anvil,
+    arbitrum,
+    arbitrumGoerli,
+    base,
+    baseSepolia,
+    mainnet,
+    optimism,
+    optimismGoerli,
+    sepolia,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [  
+        anvil,
+        arbitrum,
+        arbitrumGoerli,
+        base,
+        baseSepolia,
+        mainnet,
+        optimism,
+        optimismGoerli,
+        sepolia,
+    ],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+const WalletProvider = ({ children }: { children: React.ReactNode }) => {
+    return(
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider>
+                {children}
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    );
+};
+
+export default WalletProvider;
